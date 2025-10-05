23ff6df263b3f8e6aa50d658359e5727571e63d0
aagowl <abigail.gatsch@gmail.com>
2025-10-05 10:51:53 -0600

    add other dash files

---
+++ meteor_madness_app/action.py
@@ -0,0 +1,23 @@
+import os
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+
+bp = Blueprint('action', __name__, url_prefix='/action')
+
+def calcEvacCost(pop, daysAway, onLand):
+    cost = (461.37 + 265.9)*pop*daysAway # (average cost + economic loss)
+    if onLand:
+        cost += 20000000000
+    return cost  
+
+@bp.route('/mitigation', methods=('GET', 'POST'))
+def mitigation():
+
+    if error is None:
+            return render_template('action/mitigation.html')
+        else:
+            flash(error)
+
+    return render_template('action/mitigation.html')
+
