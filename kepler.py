051b187d6eb454da97efdda7709e4df2d8a9e4c0
CodingKraken <tristaneggenberger204@gmail.com>
2025-10-05 23:11:15 -0600

    Holy shit, the orbits work

--- kepler.py
+++ kepler.py
@@ -48,14 +48,20 @@ def cart2kepler(c: CartesianElement) -> KeplerElement:
     hmag = np.linalg.norm(h)
 
     n = np.array([-h[1], h[0], 0])  # zhat x h, ascending node vector
-    nmag = np.linalg.norm(n)
 
     vsqr = np.dot(c.v, c.v)
+    nmag = np.linalg.norm(n)
 
-    evec = 1/c.mu*((vsqr - c.mu/rmag)*c.r - np.dot(c.r, c.v)*c.v)
+    if n[1] >= 0:
+        lasc = math.acos(n[0]/nmag)
+    else:
+        lasc = 2*math.pi - math.acos(n[0]/nmag)
+
+    evec = np.cross(c.v, h)/c.mu - c.r/rmag
     ecc = np.linalg.norm(evec)
 
     E = vsqr/2 - c.mu/rmag
+
     if ecc != 1:
         sma = -c.mu/(2*E)
     else:
@@ -68,12 +74,12 @@ def cart2kepler(c: CartesianElement) -> KeplerElement:
 
     # if the inclination is small, the longitude of the ascending node is 0.
     # same goes for the argument of the periapsis with small eccentricity
-    if abs(inc) < EPSILON:
+    if abs(inc - 0) < EPSILON:
         lasc = 0
-        if abs(ecc) < EPSILON:
+        if abs(ecc - 0) < EPSILON:
             argp = 0
         else:
-            argp = math.acos(evec[0] / ecc)
+            argp = math.acos(evec[0]/ecc)
     else:
         lasc = math.acos(n[0]/nmag)
         if n[1] < 0:
@@ -81,20 +87,20 @@ def cart2kepler(c: CartesianElement) -> KeplerElement:
 
         argp = math.acos(np.dot(evec, n)/(ecc*nmag))
 
-    if abs(ecc) < EPSILON:
-        if abs(inc) < EPSILON:
+    if abs(ecc - 0) < EPSILON:
+        if abs(inc - 0) < EPSILON:
             nu = math.acos(c.r[0]/rmag)
             if c.v[0] > 0:
                 nu = 2*math.pi - nu
         else:
-            nu = math.acos(np.dot(c.r, n) / (rmag * nmag))
+            nu = math.acos(np.dot(c.r, n)/(rmag*nmag))
             if np.dot(c.v, n) > 0:
                 nu = 2*math.pi - nu
     else:
         if evec[2] < 0:
             argp = 2*math.pi - argp
 
-        nu = math.acos(np.dot(c.r, evec)/(ecc*rmag))
+        nu = math.acos(np.dot(c.r, evec)/(rmag*ecc))
         if np.dot(c.r, c.v) < 0:
             nu = 2*math.pi - nu
 
@@ -102,59 +108,40 @@ def cart2kepler(c: CartesianElement) -> KeplerElement:
 
 
 def kepler2cart(k: KeplerElement):
-    if k.ecc < 1:  # elliptical case
-        cosE = (k.ecc+math.cos(k.nu))/(1+k.ecc*math.cos(k.nu))
-        rmag = k.sma*(1-k.ecc*cosE)
-        r = rmag*np.array([math.cos(k.nu), math.sin(k.nu), 0])
-        v = math.sqrt(k.mu*k.sma)/rmag*np.array([-math.sin(math.acos(cosE)),
-                                                 math.sqrt(1-k.ecc**2)*cosE, 0])
-        rot = Rotation.from_euler("ZXZ", [-k.argp, -k.inc, -k.lasc])
-        return CartesianElement(rot.as_matrix() @ r, rot.as_matrix() @ v, k.mu)
-    if k.ecc == 1:  # parabolic case
-        return k.ecc
-
-    # hyperbolic case
-    rmag = k.sma*(1-k.ecc**2)/(1+k.ecc*math.cos(k.nu))
+    rot = Rotation.from_euler("ZXZ", [-k.argp, -k.inc, -k.lasc]).as_matrix()
+    coeff = k.sma*(1-k.ecc**2)
+    rmag = coeff/(1+k.ecc*math.cos(k.nu))
     r = rmag*np.array([math.cos(k.nu), math.sin(k.nu), 0])
-    vmag = math.sqrt(k.mu/(k.sma*(1-k.ecc**2)))
-    v = vmag*np.array([-math.sin(k.nu), k.ecc + math.cos(k.nu), 0])
-    rot = Rotation.from_euler("ZXZ", [-k.argp, -k.inc, -k.lasc])
-    return CartesianElement(rot.as_matrix() @ r, rot.as_matrix() @ v, k.mu)
+
+    v = math.sqrt(k.mu/coeff)*np.array([-math.sin(k.nu),
+                                        k.ecc+math.cos(k.nu), 0])
+    return CartesianElement(r @ rot, v @ rot, k.mu)
 
 
 def conic_from_impact(lat: np.float32, lon: np.float32, v: np.ndarray, mu, samples):
     latr = lat * math.pi/180
-    print(latr)
     theta = math.pi/2 - latr
-    print(theta * 180/math.pi)
     lonr = lon * math.pi/180
     r = Re*np.array([math.sin(theta)*math.sin(lonr),
-                     math.sin(theta)*math.cos(lonr), -math.cos(theta)])
-    print(r)
+                     math.sin(theta)*math.cos(lonr), math.cos(theta)])
     kepler = cart2kepler(CartesianElement(r, v, mu))
     if kepler.ecc < 1:
         min_anomaly = -2*math.pi
     else:
         min_anomaly = 15/16*math.acos(-1/kepler.ecc)
 
-    print(kepler.sma)
-    print(kepler.ecc)
-    print(kepler.argp)
-    print(kepler.lasc)
-    print(kepler.inc)
-    print(kepler.nu)
-    print(kepler.mu)
-    anomalies = np.linspace(min_anomaly, kepler.nu, samples)
+    anomalies = np.linspace(kepler.nu, min_anomaly, samples)
     xs = np.empty(samples)
     ys = np.empty(samples)
     zs = np.empty(samples)
+    new_kep = kepler
     for i in range(samples):
         new_kep = kepler
         new_kep.nu = anomalies[i]
         point = kepler2cart(new_kep)
-        xs[i] = point.r[0]
-        ys[i] = point.r[1]
-        zs[i] = point.r[2]
+        xs[samples-1 - i] = point.r[0]
+        ys[samples-1 - i] = point.r[1]
+        zs[samples-1 - i] = point.r[2]
 
     return (xs, ys, zs)
 
@@ -163,7 +150,7 @@ def kepler_conic(k: KeplerElement, samples):
     if k.ecc < 1:
         max_anomaly = math.pi
     else:
-        max_anomaly = 15/16*math.acos(-1/k.ecc)
+        max_anomaly = -15/16*math.acos(-1/k.ecc)
     anomalies = np.linspace(-max_anomaly, max_anomaly, samples)
     xs = np.empty(samples)
     ys = np.empty(samples)
@@ -196,16 +183,8 @@ def latlon_from_cartesian(r: np.ndarray):
 
 
 def main() -> None:
-    impact_conic = conic_from_impact(40.7128, -74.0060, np.array([3, 12, -2]),
-                                     G*Me, 1000)
-
-    # Solve for the Kepler orbit from r_init and v_init
-    #    kepler = cart2kepler(cart_init)
-    #    print(kepler.sma)
-    #    print(kepler.ecc)
-    #    print(kepler.inc)
-    #    print(kepler.argp)
-    #    print(kepler.lasc)
+    impact_conic = conic_from_impact(-40.7128, -74.0060, np.array([3, 15, 10]),
+                                     G*Me, 500)
 
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
@@ -220,7 +199,6 @@ def main() -> None:
     # Plot the surface
     ax.plot_surface(ex, ey, ez)
 
-    #    print(impact_conic[0])
     ax.plot(impact_conic[0], impact_conic[1], impact_conic[2])
     ax.plot(impact_conic[0][-1], impact_conic[1][-1], impact_conic[2][-1], c="r", marker='o')
 
